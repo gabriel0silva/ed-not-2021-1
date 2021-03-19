61446f2b30e4058fe32e095329ec174a53117cd4
gabriel0silva <pacplay07@gmail.com>
2021-03-19 01:31:17 +0000

    (18/03) Término do merge sort

--- 08-selection-sort-obj.mjs
+++ 08-selection-sort-obj.mjs
@@ -17,28 +17,30 @@ function selectionSort(vetor, fnComp) {
         return menor
     }
 
-    // Percurso até a PENÚLTIMA posição
+    // Percurso do vetor até a PENÚLTIMA posição
     for(let i = 0; i <= vetor.length - 2; i++) {
         pass++
         let menor = encontrarMenor(i + 1)
+        
         //if(vetor[menor] < vetor[i]) {
-            // Parâmetros em ordem invertida
-            if(fnComp(vetor[i], vetor[menor])) {
-            [vetor[menor], vetor[i]] = [vetor [i], vetor[menor]]
+        // Parâmetros em ordem invertida
+        if(fnComp(vetor[i], vetor[menor])) {
+            [ vetor[menor], vetor[i] ] = [ vetor[i], vetor[menor] ]
             trocas++
         }
         comps++
     }
 }
 
-// Função de comparação retorna true caso o primeiro objeto seja MAIOR que o  SEGUNDO
+// Função de comparação retorna true caso o PRIMEIRO objeto seja MAIOR que o SEGUNDO
 
 import { candidatos } from './includes/candidatos-2018.mjs'
 
-console.log('ANTES:', candidatos)
+//console.log('ANTES:', candidatos)
 console.time('Ordenando candidatos...')
 // Ordenando pelo nome de urna (NM_URNA_CANDIDATO)
 selectionSort(candidatos, (obj1, obj2) => obj1.NM_URNA_CANDIDATO > obj2.NM_URNA_CANDIDATO)
+let memoria = process.memoryUsage().heapUsed / 1024 / 1024
 console.timeEnd('Ordenando candidatos...')
 console.log('DEPOIS:', candidatos)
-console.log({trocas, pass, comps})
+console.log({trocas, pass, comps, memoria})
